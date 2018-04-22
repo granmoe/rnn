c13bcbfa53895f2a87f874aa66fd8ed25e12dfe3
Matt Granmoe <mattgranmoe@gmail.com>
2018-04-21 23:23:20 -0500

    Destructure exports explicitly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export * from './utils'
-export * from './RNN' // TODO: destructure these explicitly
+export { updateMats, assert, randi, randf, maxi, samplei, softmax } from './utils'
+export { initRNN, forwardRNN, initLSTM, forwardLSTM } from './RNN'
 export { default as Mat, RandMat } from './Mat'
 export { default as Graph } from './Graph'
 export { default as Solver } from './Solver'
